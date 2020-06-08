e377801a1dd21684680afcbe99bf1d9c95d4250e
yelin2 <yelin120993@gmail.com>
2020-06-09 02:42:49 +0900

    add dataparallel

--- train_models.py
+++ train_models.py
@@ -2,16 +2,23 @@ import torch.nn as nn
 import torch
 import torchvision
 import numpy as np
+import time
 
 from models.style_G import G_mapping
 
-# convert to GPU operation
+# TODO study multi-gpu 
 def main():
+    curr = time.time()
     G = G_mapping()
-    latent = np.random.randn(4, 512)
-    img = np.random.randn(4, 3, 64, 64)
-    res = G(torch.from_numpy(latent).float())
-    print(res.shape)
+    G_parallel = torch.nn.DataParallel(G, device_ids=[0, 1, 2, 3]).to('cuda')
+    
+    for i in range(100):
+        curr = time.time()
+        latent = np.random.randn(4, 512)
+        res = G_parallel(torch.from_numpy(latent).float())
+        print(res.shape)
+        print(time.time()-curr)
+        
 
 if __name__ == '__main__':
     main()
