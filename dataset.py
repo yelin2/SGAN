c856e1817bec94c531e3dc00e86f0938d2744e3c
bullgom <bullgom0213@gmail.com>
2020-06-09 16:18:59 +0900

    llvl 사용하지 않고 바로 resolution 사용

--- dataset.py
+++ dataset.py
@@ -9,14 +9,14 @@ import os
 
 
 class MultiResDataset(Dataset):
-    def __init__(self, root_dir, transform=None, llvl=8):
+    def __init__(self, root_dir, transform=None, resolution=8):
         # layer level
         self.root_dir = root_dir
         self.transform = transform
-        self.llvl = llvl
+        self.resolution = resolution
 
-    def __len__(self):
-        return 70000
+    def __len__(self): 
+        return 70000 #! 좀 더 사람답게 바꾸자 ^^
 
     def __getitem__(self, index):
         low_num = str(index % 1000).zfill(3)
@@ -30,14 +30,13 @@ class MultiResDataset(Dataset):
         return TF.to_tensor(img)
 
     @property
-    def llvl(self):
-        return self._llvl
+    def resolution(self):
+        return self._resolution
     
-    @llvl.setter
-    def llvl(self, value):
+    @resolution.setter
+    def resolution(self, value):
         assert 0 < value
-        self._llvl = value
-        self.resolution = 2 ** self._llvl
+        self._resolution = value
         self.transform_resize = transforms.Resize((self.resolution, self.resolution))
 
 
