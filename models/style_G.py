a052a715419118abac40b9e989326b99f9450dda
yelin2 <yelin120993@gmail.com>
2020-06-09 17:13:32 +0900

    add G_synthesis first block

--- models/style_G.py
+++ models/style_G.py
@@ -54,30 +54,85 @@ class G_mapping(nn.Module):
 
         return w
     
-class extract_style(nn.Module):
-    def __init__(self, dlatent_size=512, feat_c=3):
-        super.__init__()
-        self.transformation = nn.Linear(dlatent_size, 2*feat_c)
-    def forward(self, x):      
-        return 1
+def normalize(x, eps=1e-8):
+    return x * torch.rsqrt(torch.mean(x, dim=1, keepdim=True) + eps)
 
-class noise(nn.Module):
-    def __init__(self):
-        super.__init__()
-    def forward(self):
-        return 1
+class apply_noise(nn.Module):
+    def __init__(self, feat_size):
+        # size: weight size = channel of feature map
+        super().__init__()
+        self.weight = torch.randn(feat_size, requires_grad=True)
+
+    def forward(self, x, blocks):
+        # make noise
+        noise = torch.randn(size=(1, 1, 2**blocks, 2**blocks))
+        # print(noise.size(),"= [1, 1, 8, 8]")
+        # print(self.weight.size(),"= [8]")
+        # TODO reshape noise
+        # add noise
+        x = x + noise*self.weight.reshape([1, -1, 1, 1])
+        return x
 
 class AdaIN(nn.Module):
-    def __init__(self):
-        super.__init__()
-    def forward(self):
-        return 1
+    def __init__(self, dlatent_size, feat_size):
+        super().__init__()
+        self.dlatent_size = dlatent_size
+        self.feat_size = feat_size
+        self.transformation = nn.Linear(dlatent_size, 2*feat_size)
+
+    def instance_norm(self, x, eps=1e-8):
+        return (x-torch.mean(x, dim=[2,3], keepdim=True))*torch.rsqrt(torch.mean(x, dim=[2,3], keepdim=True) + eps)
+
+    def style_mod(self, x, dlatent):
+        # dlatent shape [4, 1, 512]
+        # style shape [4, 2*512]
+        style = self.transformation(dlatent)
+        # style shape [b, 2, feat_size, 1, 1] [4, 2, 512, 512, 1, 1]
+        style = style.reshape([-1, 2, self.feat_size] + [1]*2)
+        # style 적용
+        return x * (style[:,0]+1) + style[:,1]
+
+    def forward(self, x, dlatent):
+        x = self.instance_norm(x)
+        x = self.style_mod(x, dlatent)
+        return x
+ 
 
 class block(nn.Module):
-    def __init__(self):
-        super.__init__()
-    def forward(self):
-        return 1
+    def __init__(self, blocks=3, dlatent_size = 512):
+        super().__init__()
+        self.blocks = blocks
+        self.dlatent_size = dlatent_size
+        self.feat_size = min(int(8192/(2**blocks)), 512)
+        self.apply_noise = apply_noise(self.feat_size)
+
+        if blocks != 3:
+            # self.conv1 = nn.Conv2d()
+            pass
+        else:
+            self.const = torch.ones((1, dlatent_size, 2**blocks, 2**blocks), requires_grad=True)
+        self.AdaIN = AdaIN(self.dlatent_size, self.feat_size)
+        self.conv2 = nn.Conv2d(in_channels=dlatent_size, out_channels=dlatent_size, kernel_size=3, padding=1) 
+        self.lrelu = nn.LeakyReLU(0.2)
+        
+
+    def forward(self, w, x = None):
+        if self.blocks != 3 or x != None:
+            x = self.conv1(x)
+        else:
+            x = self.const
+        # print(x.size(), "= [1, 512, 8, 8]")
+        x = self.apply_noise(x, blocks=self.blocks)
+        x = normalize(self.lrelu(x))
+        x = self.AdaIN(x, w)
+        '''
+        
+        x = self.conv2(x)
+        x = self.apply_noise()
+        x = normalize(self.lrelu(x))
+        x = self.style_mod2(self.instance_norm(x))
+        '''
+        return x
 
 class G_synthesis(nn.Module):
     def __init__(self, dlatent_size=512, num_channels=3, resolution=512, blur_filter=[1,2,1]):
@@ -88,14 +143,14 @@ class G_synthesis(nn.Module):
             resolution: Output resolution. default 512
             blur_filter: Low-pass filter to apply when resampling activations. default [1,2,1] None = no filtering. 
 
-        fmap_base           = 8192,         # Overall multiplier for the number of feature maps.
-        fmap_decay          = 1.0,          # log2 feature map reduction when doubling the resolution.
-        fmap_max            = 512,          # Maximum number of feature maps in any layer.
-        fused_scale         = 'auto',       # True = fused convolution + scaling, False = separate ops, 'auto' = decide automatically.
-        is_template_graph   = False,        # True = template graph constructed by the Network class, False = actual evaluation.
-        force_clean_graph   = False,        # True = construct a clean graph that looks nice in TensorBoard, False = default behavior.
+            fmap_base           = 8192,         # Overall multiplier for the number of feature maps.
+            fmap_decay          = 1.0,          # log2 feature map reduction when doubling the resolution.
+            fmap_max            = 512,          # Maximum number of feature maps in any layer.
+            fused_scale         = 'auto',       # True = fused convolution + scaling, False = separate ops, 'auto' = decide automatically.
+            is_template_graph   = False,        # True = template graph constructed by the Network class, False = actual evaluation.
+            force_clean_graph   = False,        # True = construct a clean graph that looks nice in TensorBoard, False = default behavior.
         
-        dtype = fp32, use instance norm, use pixel norm, use_wscale?, use leakyrelu, noise 매번 만들기, use noise , learning constant, use styles
+            dtype = fp32, use instance norm, use pixel norm, use_wscale?, use leakyrelu, noise 매번 만들기, use noise , learning constant, use styles
         '''
         super().__init__()
         self.dlatent_size = dlatent_size
@@ -103,11 +158,9 @@ class G_synthesis(nn.Module):
         self.resolution = resolution
         self.blur_filter = blur_filter
 
+        self.block1 = block(blocks=3, dlatent_size=512)
 
-
-
-
-    def forward(self, dlatents_in, noise_in, layer_level):
+    def forward(self, dlatents_in, layer_level, noise_in=None):
         '''
         Arguments
             dlatents_in: style vector w [b, #layers(14), 512]
@@ -115,10 +168,11 @@ class G_synthesis(nn.Module):
         '''
         images_out = 3
         # 8x8
-            # do first block 
         if layer_level == 3:
+            # w [b, 14, 512]에서 [b, 2, 512]만 w로 넘겨줌
+            images_out = self.block1(w=dlatents_in[:, 0])
             return images_out
-        
+        '''
         # 16x16
             # do block
         if layer_level == 4:
@@ -149,6 +203,7 @@ class G_synthesis(nn.Module):
         if layer_level == 9:
             # fade_in
             return images_out
+        '''
 
-
+        return images_out
 
--- train_models.py
+++ train_models.py
@@ -4,21 +4,20 @@ import torchvision
 import numpy as np
 import time
 
-from models.style_G import G_mapping
+from models.style_G import G_mapping, G_synthesis
 
 # TODO study multi-gpu 
 def main():
-    curr = time.time()
     G = G_mapping()
-    G_parallel = torch.nn.DataParallel(G, device_ids=[0, 1, 2, 3]).to('cuda')
-    
-    for i in range(100):
-        curr = time.time()
-        latent = np.random.randn(4, 512)
-        res = G_parallel(torch.from_numpy(latent).float())
-        print(res.shape)
-        print(time.time()-curr)
-        
+    G_syn = G_synthesis(dlatent_size=512, num_channels=3, resolution=512, blur_filter=[1,2,1])
+    # G_parallel = torch.nn.DataParallel(G, device_ids=[0, 1, 2, 3]).to('cuda')
+
+    latent = torch.randn((4, 512))
+    # res = G_parallel(torch.from_numpy(latent).float())
+    res = G(latent.float())
+    res1 = G_syn(res, layer_level=3)
+    print(res.shape)
+    print(res1.shape)
 
 if __name__ == '__main__':
     main()
