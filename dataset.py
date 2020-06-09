415bf0882cc56b976349b6a8a9e47519d4a44280
bullgom <bullgom0213@gmail.com>
2020-06-09 13:58:47 +0900

    transform 관련 수정

--- dataset.py
+++ dataset.py
@@ -1,26 +1,43 @@
+import torchvision.transforms.functional as TF
 from torch.utils.data import Dataset
 from torchvision import transforms
-from skimage import io
+from PIL import Image
 import numpy as np
 import os
 
 # transform에 resize 추가하지 마셈
+
+
 class MultiResDataset(Dataset):
     def __init__(self, root_dir, transform=None, llvl=8):
         # layer level
         self.root_dir = root_dir
         self.transform = transform
         self.llvl = llvl
-    
+
     def __len__(self):
         return 70000
-    
+
     def __getitem__(self, index):
         low_num = str(index % 1000).zfill(3)
         high_num = str(int(index / 1000)).zfill(2)
         img_path = self.root_dir + "/" + high_num + "000/" + high_num + low_num + ".png"
-        img = io.imread(img_path)
+        img = Image.open(img_path)
+        img = self.transform_resize(img)
         if self.transform:
-            img = transforms.Resize((self.resolution, self.resolution))
             img = self.transform(img)
-        return img
+        
+        return TF.to_tensor(img)
+
+    @property
+    def llvl(self):
+        return self._llvl
+    
+    @llvl.setter
+    def llvl(self, value):
+        assert 0 < value
+        self._llvl = value
+        self.resolution = 2 ** self._llvl
+        self.transform_resize = transforms.Resize((self.resolution, self.resolution))
+
+
